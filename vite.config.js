513a27714f965b9e1a35cc668f8de9ba625304a7
Andre <adfariacarvalho@gmail.com>
2026-06-14 13:18:49 -0300

    feat: vite multi-page config + local env for WS URL

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: "./",
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        spectator: resolve(__dirname, 'spectator.html'),
+      },
+    },
+  },
   test: {
-    environment: "node",
+    environment: 'node',
   },
 });
